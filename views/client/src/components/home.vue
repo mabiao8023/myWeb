547fb2142cdb3cc37737d1916f64f6460cf78c6c
mabiao <mabiao@linghit.com>
2017-12-21 17:53:47 +0800

    前端架构搭建

---
+++ views/client/src/components/home.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="hello">
+    <el-switch
+    v-model="value"
+    active-color="#13ce66"
+    inactive-color="#ff4949">
+  </el-switch>
+  <!-- 轮播插件 -->
+    <el-carousel :interval="5000" arrow="always">
+    <el-carousel-item v-for="item in 4" :key="item">
+      <h3>{{ item }}</h3>
+    </el-carousel-item>
+  </el-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      value: true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.el-carousel__item h3 {
+    color: #475669;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 300px;
+    margin: 0;
+  }
+  
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+</style>
--- views/client/src/main.js
+++ views/client/src/main.js
@@ -3,8 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'normalize.css'
+import { 
+	Switch,
+	Carousel,
+  	CarouselItem,
+} from 'element-ui'
 
 Vue.config.productionTip = false
+
+Vue.use(Switch)
+Vue.use(Carousel)
+Vue.use(CarouselItem)
 
 /* eslint-disable no-new */
 new Vue({
